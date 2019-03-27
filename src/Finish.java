fc78e0bd5f24a8d8903fd5994c37315c035e7b1b
A1M4N3 <aimanex64@gmail.com>
2019-03-27 21:36:21 +0100

    update

--- src/Finish.java
+++ src/Finish.java
@@ -1,6 +1,14 @@
 public class Finish extends VeldObject {
 
+	public Finish(int rij, int kolom) {
+		super(rij, kolom);
+	}
+
 	public void spelGewonnen() {
 	}
 
+	@Override
+	public boolean padVrij() {
+		return true;
+	}
 }
--- src/Speler.java
+++ src/Speler.java
@@ -3,9 +3,14 @@ public class Speler extends VeldObject {
 	private Sleutel zak;
 
 	public Speler(int rij, int kolom) {
+		super(rij, kolom);
 	}
 
 	public void sleutelPakken() {
 	}
 
+	@Override
+	public boolean padVrij() {
+		return false;
+	}
 }
