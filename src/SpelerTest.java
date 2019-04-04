76fe1888dc777bbcacf944726f7e0046db6124a7
A1M4N3 <aimanex64@gmail.com>
2019-04-05 00:02:48 +0200

    update

--- src/SpelerTest.java
+++ src/SpelerTest.java
@@ -14,45 +14,67 @@ public class SpelerTest {
 
     @Test
     public void lopenRechts() {
-        speelveld.getSpeler().lopen(1,"O");
-        assertEquals(1,speelveld.getSpeler().getKolom());
-    }
-    @Test
-    public void lopenBeneden() {
-        speelveld.getSpeler().lopen(1,"Z");
-        assertEquals(1,speelveld.getSpeler().getRij());
+        speelveld.getSpeler().setRij(1);
+        speelveld.getSpeler().lopen(1, "O");
+        assertEquals(1, speelveld.getSpeler().getKolom());
     }
 
     @Test
-    public void testStartpositieSpelerKolom(){
+    public void lopenBeneden() {
+        speelveld.getSpeler().lopen(1, "Z");
+        assertEquals(1, speelveld.getSpeler().getRij());
+    }
+
+    @Test
+    public void lopenLinks() {
+        speelveld.getSpeler().setRij(1);
+        speelveld.getSpeler().setKolom(1);
+        speelveld.getSpeler().lopen(-1, "W");
         assertEquals(0, speelveld.getSpeler().getKolom());
     }
+
     @Test
-    public void testStartpositieSpelerRij(){
+    public void lopenBoven() {
+        speelveld.getSpeler().setRij(2);
+        speelveld.getSpeler().lopen(-1, "N");
+        assertEquals(1, speelveld.getSpeler().getRij());
+    }
+
+    @Test
+    public void testStartpositieSpelerKolom() {
+        assertEquals(0, speelveld.getSpeler().getKolom());
+    }
+
+    @Test
+    public void testStartpositieSpelerRij() {
         assertEquals(0, speelveld.getSpeler().getRij());
     }
 
     @Test
     public void checkVeldObject() {
+        speelveld.getSpeler().checkVeldObject(1,"Z");
     }
 
     @Test
     public void sleutelPakken() {
+        speelveld.getSpeler().setRij(1);
+        speelveld.getSpeler().setKolom(3);
+        speelveld.getSpeler().lopen(1, "O");
+        assertNotNull(speelveld.getSpeler().getZak());
     }
 
     @Test
     public void setZak() {
+        speelveld.getSpeler().setZak(new Sleutel(100, 0, 0));
+        assertNotNull(speelveld.getSpeler().getZak());
     }
 
-    @Test
-    public void getZak() {
-    }
 
     @Test
-    public void setRij() {
-    }
-
-    @Test
-    public void setKolom() {
+    public void setRijEnKolom() {
+        speelveld.getSpeler().setRij(1);
+        speelveld.getSpeler().setKolom(1);
+        assertEquals(1, speelveld.getSpeler().getRij());
+        assertEquals(1, speelveld.getSpeler().getKolom());
     }
 }
